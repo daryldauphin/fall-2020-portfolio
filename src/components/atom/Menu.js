44a016b5335804fdcff5e76812ae1954c7b977bd
Daryl <mrdaryldauphin@gmail.com>
2020-08-26 22:25:55 -0400

    finishing mobile menu

--- src/components/atom/Menu.js
+++ src/components/atom/Menu.js
@@ -1,52 +1,54 @@
-import React,{useRef, useEffect, useState} from 'react'
-import './About.scss';
-import './Blob.scss';
-import './Menu.scss';
-import gsap, {Power4, TimelineMax, Power2} from 'gsap'
-import $ from "jquery";
+import React, { useRef, useEffect, useState } from "react";
+import "./About.scss";
+import "./Blob.scss";
+import "./Menu.scss";
+import { gsap } from "gsap";
 
 function Menu() {
+  const [active, setActive] = useState(false);
 
-  const [state, setState] = useState({
-    active: null
-  })
+  const handleClick = () => {
+    setActive(!active);
+  };
 
-const handleClick = () => {
-    setState({
-    active: !state.active
-})
+  const bar1 = useRef();
+  const bar2 = useRef();
+  const bar3 = useRef();
+  const tl = useRef();
 
-  }
+  useEffect(() => {
+    tl.current = gsap
+      .timeline({
+        paused: true,
+        defaults: {
+          ease: "power4.in",
+          duration: 0.4,
+          
+        }
+      })
+      .to(bar1.current, { x: "+=80", y: "-=80" }, 0.1)
+      .to(bar2.current, { x: "+=80", y: "-=80" }, 0)
+      .to(bar3.current, { x: "+=80", y: "-=80" }, 0.2)
+    return () => tl.current.kill();
+  }, []);
 
-let bar1, bar2, bar3 = useRef(null)
-
-useEffect(()=>{
-    if(state.active === true){
-        gsap.to(bar1, 0.4, { x: "+=80px", y: "-=80px", delay: 0.1, ease: Power4.easeIn })
-        gsap.to(bar2, 0.4, {  x: "+=80px", y: "-=80px", ease: Power4.easeIn})
-        gsap.to(bar3, 0.4, { x: "+=80px", y: "-=80px", delay: 0.2, ease: Power4.easeIn})
-    }else if (state.active.reversed()){
-      gsap.to(bar1, 0.4, { x: "+=80px", y: "-=80px", delay: 0.1, ease: Power4.easeIn }).reverse()
-        gsap.to(bar2, 0.4, {  x: "+=80px", y: "-=80px", ease: Power4.easeIn}).reverse()
-        gsap.to(bar3, 0.4, { x: "+=80px", y: "-=80px", delay: 0.2, ease: Power4.easeIn}).reverse()
+  useEffect(() => {
+    if (active) {
+      tl.current.play();
+    } else {
+      tl.current.reverse();
     }
-})
-    
+  }, [active, tl]);
 
   return (
-      <>
-    <span onClick={handleClick} class="menu-trigger">
-      <i  ref={(el) => {bar1 = el}}class="menu-trigger-bar top"></i>
-      <i ref={(el) => {bar2 = el}}class="menu-trigger-bar middle"></i>
-      <i ref={(el) => {bar3 = el}}class="menu-trigger-bar bottom"></i>
-    </span>
-    <span class="close-trigger">
-      <i class="close-trigger-bar left"></i>
-      <i class="close-trigger-bar right"></i>
-    </span>
-    
-  </>
-    
+    <>
+      <span onClick={handleClick} className="menu-trigger">
+        <i ref={bar1} className="menu-trigger-bar top"></i>
+        <i ref={bar2} className="menu-trigger-bar middle"></i>
+        <i ref={bar3} className="menu-trigger-bar bottom"></i>
+      </span>
+      
+    </>
   );
 }
 
