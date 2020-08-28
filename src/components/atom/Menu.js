650ec9d4f67590d5a259db7a53170fc3fb5db2fa
Daryl <mrdaryldauphin@gmail.com>
2020-08-28 08:27:37 -0400

    finishing mobile menu

--- src/components/atom/Menu.js
+++ src/components/atom/Menu.js
@@ -2,43 +2,87 @@ import React, { useRef, useEffect, useState } from "react";
 import "./About.scss";
 import "./Blob.scss";
 import "./Menu.scss";
-import { gsap } from "gsap";
+import { gsap, TimelineMax, Power2, Power4 } from "gsap";
 
 function Menu() {
   const [active, setActive] = useState(false);
 
   const handleClick = () => {
     setActive(!active);
+    toggle = !toggle;
   };
 
   const bar1 = useRef();
   const bar2 = useRef();
   const bar3 = useRef();
   const tl = useRef();
-
+  var menuAnimation = new TimelineMax({paused:true});
+  var menuAnimationBack = new TimelineMax({paused:true, reversed: true});
+  
   useEffect(() => {
     tl.current = gsap
       .timeline({
         paused: true,
         defaults: {
           ease: "power4.in",
-          duration: 0.4,
+          duration: 0.9,
           
         }
       })
       .to(bar1.current, { x: "+=80", y: "-=80" }, 0.1)
       .to(bar2.current, { x: "+=80", y: "-=80" }, 0)
       .to(bar3.current, { x: "+=80", y: "-=80" }, 0.2)
+      .reverse(0); 
     return () => tl.current.kill();
   }, []);
 
   useEffect(() => {
     if (active) {
-      tl.current.play();
+      tl.current.play().reverse(2);
+      menuAnimation.play(0)
     } else {
-      tl.current.reverse();
+      menuAnimationBack.play(0)
+      tl.current.reverse(0);
     }
-  }, [active, tl]);
+  }, [active, tl, menuAnimation, menuAnimationBack]);
+
+
+  console.clear();
+  
+  var navMain = document.getElementById("nav-main");
+  var menuButton = document.getElementById("menu-button");
+  var toggle = true;
+  var mqls = [
+   window.matchMedia("(min-width: 769px)"),
+   window.matchMedia("(min-width: 581px) and (max-width: 768px)"),
+   window.matchMedia("(max-width: 580px)")
+  ]
+  
+  function mediaqueryresponse(mql){
+      if (mqls[0].matches){
+      menuAnimation
+          .to(navMain, 0.8, {width: '100%', className : "+=vertical", ease: Power2.easeInOut, transform: "translate3d(0,0,0)"},0);
+      }
+      if (mqls[1].matches){
+          menuAnimation
+          .to(navMain, 0.8, {width: '50%', className : "+=vertical", ease: Power2.easeInOut, transform: "translate3d(0,0,0)"},0);
+      }
+      if (mqls[2].matches){
+          menuAnimation
+          .to(navMain, 0.8, {width: '100%', className : "+=vertical", ease: Power2.easeInOut, transform: "translate3d(0,0,0)"},0);
+      }
+  }
+  
+  for (var i=0; i<mqls.length; i++){
+   mediaqueryresponse(mqls[i])
+   mqls[i].addListener(mediaqueryresponse)
+  }
+  
+  menuAnimationBack
+  .to(navMain, 0.55, {width: 0, className : "+=skewback", ease: Power4.easeIn, transform: "translate3d(0,0,0)"},0)
+   
+ 
+  
 
   return (
     <>
@@ -46,7 +90,18 @@ function Menu() {
         <i ref={bar1} className="menu-trigger-bar top"></i>
         <i ref={bar2} className="menu-trigger-bar middle"></i>
         <i ref={bar3} className="menu-trigger-bar bottom"></i>
-      </span>
+        </span>
+        
+        <nav id="nav-main" class="skew">      
+
+         <section id="home">Home</section>
+        </nav>
+   <button id="menu-button" class="menu-button">
+        <div class="menu-bars" id="menuBox">
+        </div>
+  </button>
+
+
       
     </>
   );
